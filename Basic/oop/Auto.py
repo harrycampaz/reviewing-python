6b6935c8ed51e61dc6563d038d15aa8a8dbe1c62
harrycampaz <harrycampaz@gmail.com>
2020-02-22 17:49:26 -0500

    start second class

--- Basic/oop/Auto.py
+++ Basic/oop/Auto.py
@@ -7,8 +7,16 @@ class Auto():
     def arrancar(self):
         self.enmarcha = True
 
+    def estado(self):
+        if self.enmarcha:
+            return "El coche esta en marcha"
+        else:
+            return "El Coche esta parado"
+
 
 miCoche = Auto()
 miCoche.arrancar()
 print("Tipo de coche: " + miCoche.typo + " Y esta en Marcha: " + str(miCoche.enmarcha))
+
+print(miCoche.estado())
 
