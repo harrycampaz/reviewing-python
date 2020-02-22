658beea82ba3715d13cfbcecae0260f32aa245ec
harrycampaz <harrycampaz@gmail.com>
2020-02-22 18:19:22 -0500

    encapsulacion python

--- Basic/oop/Auto.py
+++ Basic/oop/Auto.py
@@ -1,22 +1,44 @@
 class Auto():
-    color = "rojo"
-    marca = "BMW"
-    typo = "Electrico"
-    enmarcha = False
 
-    def arrancar(self):
-        self.enmarcha = True
+    def __init__(self):
+        self.__color = "rojo"
+        self.__marca = "BMW"
+        self.__typo = "Electrico"
+        self.__enmarcha = False
+
+    def arrancar(self, arrancar):
+
+        self.__enmarcha = arrancar
+        if self.__enmarcha:
+            chequeo = self.__chequeoInterno()
+
+        if self.__enmarcha and chequeo:
+             return "El coche esta en marcha"
+        elif self.__enmarcha and chequeo == False:
+            return "Algo a paso en el chequeo"
+        else:
+             return "El Coche esta parado"
+
 
     def estado(self):
-        if self.enmarcha:
-            return "El coche esta en marcha"
-        else:
-            return "El Coche esta parado"
+       print(f" El choche es marca {self.__marca} y color {self.__color}")
+           
+
+    def __chequeoInterno(self):
+        print("Realizado chequeo interno")
+        self.gasolina = "ok"
+        self.aceite= "ok"
+        self.puertas = "cerradas"
+
+        if self.gasolina == "ok" and self.aceite == "ok" and self.puertas == "cerradas":
+            return True
+        else : 
+            return False
 
 
 miCoche = Auto()
-miCoche.arrancar()
-print("Tipo de coche: " + miCoche.typo + " Y esta en Marcha: " + str(miCoche.enmarcha))
+print(miCoche.arrancar(True))
+# print("Tipo de coche: " + miCoche.typo + " Y esta en Marcha: " + str(miCoche.enmarcha))
 
-print(miCoche.estado())
+miCoche.estado()
 
