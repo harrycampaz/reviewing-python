59d1947f6e67301caa30bb02ddca968aafbf3ebb
harrycampaz <harrycampaz@gmail.com>
2020-02-25 23:24:10 -0500

    almost done

--- Basic/oop/herencia/herenciasuper.py
+++ Basic/oop/herencia/herenciasuper.py
@@ -6,13 +6,21 @@ class Persona():
         self.lugarResidencia  = lugarResidencia
 
     def descripcion(self):
-        print(f"Nombre {self.nombre} , edad: {edad} y Lugar de resedencia: {self.lugarResidencia}")
+        print(f"Nombre {self.nombre} , edad: {self.edad} y Lugar de resedencia: {self.lugarResidencia}")
 
-class Empleado():
-    def __init__(self, salario, antiguedad):
+class Empleado(Persona):
+    def __init__(self, salario, antiguedad, nombre, edad, residencia):
+        super().__init__(nombre , edad , residencia)
         self.salario = salario
         self.antiguedad = antiguedad
 
 
 
-        
+
+harry = Persona("Harry", 23, "Tura")
+
+harry.descripcion()
+
+ender = Empleado(10000, 23, "ander", 32, "Medellin")
+
+ender.descripcion()
